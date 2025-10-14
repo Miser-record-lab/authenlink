5d5d3a0ee213ba87955e460212b4b9ad37039290
Hugo <hugomisery1@gmail.com>
2025-10-14 14:17:50 +0200

    add blog

--- data/navbar.ts
+++ data/navbar.ts
@@ -2,4 +2,5 @@ export const navItems = [
   { name: "Acceuil", link: "#acceuil" },
   { name: "Services", link: "#services" },
   { name: "Méthode", link: "#Collaboration" },
+  { name: "Blog", link: "https://blog.authenlink.com/" },
 ];
