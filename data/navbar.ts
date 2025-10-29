e945ffc9085302ca7ba1f773859907a7b4222679
Authenlink <authenlink@gmail.com>
2025-10-29 18:58:24 +0100

    blog and docs

--- data/navbar.ts
+++ data/navbar.ts
@@ -2,4 +2,6 @@ export const navItems = [
   { name: "Acceuil", link: "#acceuil" },
   { name: "Services", link: "#services" },
   { name: "Méthode", link: "#Collaboration" },
+  { name: "Documentation", link: "https://www.docs.authenlink.com" },
+  { name: "Blog", link: "https://www.blog.authenlink.com" },
 ];
