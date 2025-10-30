b20df2106434a0ea21ac06338b479cc472aa1219
Authenlink <authenlink@gmail.com>
2025-10-30 10:25:14 +0100

    logo

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -237,7 +237,7 @@ export const NavbarLogo = () => {
       href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-foreground"
     >
-      <Image src="/authenlink.png" alt="logo" width={30} height={30} priority />
+      <Image src="/authenlink.png" alt="logo" width={40} height={40} priority />
       <span className="font-medium text-foreground">AuthenLink</span>
     </Link>
   );
